7122de8200d8386eb075942065bb7bd8428a3a9d
aswanth14-pixel <aswanthnarayan2615@gmail.com>
2026-03-16 14:28:59 +0530

    Initial commit

---
+++ src/BookMyStay.java
@@ -0,0 +1,13 @@
+
+public class UseCase1HotelBookingApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("  Welcome to Hotel Booking System");
+        System.out.println("=================================");
+        System.out.println("Application has started successfully!");
+
+    }
+}
